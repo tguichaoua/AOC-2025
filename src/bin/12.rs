61623ce17ea52871abd0c110fd15e072c56eb4fa
Tristan Guichaoua <tguichaoua.dev@gmail.com>
2025-12-12 07:14:30 +0100

    🚨lints

--- src/bin/12.rs
+++ src/bin/12.rs
@@ -1,10 +1,10 @@
 advent_of_code::solution!(12);
 
-pub fn part_one(input: &str) -> Option<u64> {
+pub fn part_one(_input: &str) -> Option<u64> {
     None
 }
 
-pub fn part_two(input: &str) -> Option<u64> {
+pub fn part_two(_input: &str) -> Option<u64> {
     None
 }
 
